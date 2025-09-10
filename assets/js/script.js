d149a869241e4a08839ad305dc778e02d8af1f12
fullyoo19 <fullyoo19@gmail.com>
2025-09-10 15:37:50 +0900

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+
+// var 변수선언
+var a1 = 1;
+var a2 = 2;
+
+// "" 쌍따옴표는 문자로 인식
+//자바스크립트는 숫자(정수, 실수), 문자
+// +, -, x, /, *
+// 숫자(number), 문자(string), 심볼(symbol)
+// object, boolean
+
+document.write(a1);
+
+document.write(a1 + a2 + + a2 + "<br>");
+
+document.write(a1 + a1 + "<br>");
+
+document.write(1 + "2" + "<br>");
+
+document.write("문자열" + a2);
+
+document.write(" 문자열열" + "<br>");
+
+document.write("문자문자" + "<br>");
+
+
+var a1 = 5;
+document.write(a1 + "<p>");
+
+
+
+let loginBtn = document.querySelector('.login');
+
+// alert('ㅋㅋ');
+
+
+loginBtn.addEventListener("click", function () {
+    alert('ㅋㅋz');
+    prompt("아이디를 입력해주세요")
+
+})
+
+loginBtn.addEventListener("click", function () {
+
+})
+
+
+
+
+
+
